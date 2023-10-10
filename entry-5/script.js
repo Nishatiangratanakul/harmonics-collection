249812c4923b0885ba9d1612a9aec40b5e88ca9c
Nishatiangratanakul <chefnisha4116@gmail.com>
2023-10-09 23:22:53 -0400

    entry 4 updates + entry5

---
+++ entry-5/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const gridItems = document.querySelectorAll(".grid-item");
+
+    gridItems.forEach((gridItem) => {
+        const popout = gridItem.querySelector(".popout");
+        const closePopoutBtn = gridItem.querySelector(".close-popout");
+
+        // Show the popout when the grid item is clicked
+        gridItem.addEventListener("click", (e) => {
+            e.stopPropagation(); // Prevent click event from propagating to the container
+            popout.style.display = "block";
+        });
+
+        // Close the popout when the "Close" button is clicked
+        closePopoutBtn.addEventListener("click", (e) => {
+            e.stopPropagation(); // Prevent click event from propagating to the container
+            popout.style.display = "none";
+        });
+    });
+
+    // Close popouts when clicking anywhere outside of them
+    document.addEventListener("click", (e) => {
+        gridItems.forEach((gridItem) => {
+            const popout = gridItem.querySelector(".popout");
+            if (popout && e.target !== gridItem && !gridItem.contains(e.target)) {
+                popout.style.display = "none";
+            }
+        });
+    });
+});
